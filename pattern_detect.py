dfe04efaa862aab225bc9802198b46b675f12a4e
motiz007 <francismototo@gmail.com>
2024-04-21 12:20:41 +0300

    fx screener muti timeframe

--- pattern_detect.py
+++ pattern_detect.py
@@ -1,19 +1,26 @@
 import talib
 import yfinance as yf
+import pandas as pd
 from datetime import datetime, timedelta
 
 end_date = datetime.now().date()
 start_date = end_date - timedelta(days=5)
+intervals = ["15m", "1h", "90m", "1d"]
+pair_stats = {}
 
-data = yf.download("EURUSD=X", start=start_date, end=end_date, interval="15m")
+for timeframe in intervals:
+    data = yf.download("EURUSD=X", start=start_date, end=end_date, interval=timeframe)
 
-morning_star = talib.CDLMORNINGSTAR(data['Open'], data['High'], data['Low'], data['Close'])
+    engulfing = talib.CDLENGULFING(data['Open'], data['High'], data['Low'], data['Close'])
 
-engulfing = talib.CDLENGULFING(data['Open'], data['High'], data['Low'], data['Close'])
+    data['Engulfing'] = engulfing
 
-data['Morning Star'] = morning_star
-data['Engulfing'] = engulfing
+    engulfing_days = data[data['Engulfing'] != 0]
+    pair_stats[timeframe] = engulfing_days
 
-engulfing_days = data[data['Engulfing'] != 0]
+    #print(f"Timeframe: {timeframe}")
+    #print(engulfing_days[['Engulfing']])
 
-print(engulfing_days)
+for key, value in pair_stats.items():
+    print(f"{key}:")
+    print(value[['Engulfing']])
